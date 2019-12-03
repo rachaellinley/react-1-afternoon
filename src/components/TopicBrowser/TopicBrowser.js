49b8a9a454c8ac4fa3ee987ece9dc96b86268344
Rachael Sultzman <devmountainstudent@gmail.com>
2019-12-03 13:31:03 -0600

    react day one

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import EvenAndOdd from '../Topics/EvenAndOdd'
+import FilteredObject from '../Topics/FilterObject'
+import FilterString from '../Topics/FilterString'
+import Palindrome from '../Topics/Palindrome'
+import Sum from '../Topics/Sum'
+
+export default class TopicBrowser extends Component {
+  render() {
+    return (
+       <div>
+           <EvenAndOdd />
+           <FilteredObject />
+           <FilterString/>
+           <Palindrome/>
+           <Sum/>
+       </div>
+    )
+  }
+}
